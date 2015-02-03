84e982f1af22390bc75e1d0c43ed0d45d0c677ae
sweetticket <yk494@cornell.edu>
2015-02-02 23:27:22 -0500

    footer links

--- p1/template/footer.php
+++ p1/template/footer.php
@@ -1,7 +1,7 @@
 <footer>
-                <img src="img/facebook.png" alt="facebook" id="facebook-icon">
-                <img src="img/github.png" alt="github" id="github-icon">
-                <img src="img/linkedin.png" alt="linkedin" id="linkedin-icon">
+                <a href="http://facebook.com/ki.jenn"><img src="img/facebook.png" alt="facebook" id="facebook-icon"></a>
+                <a href="http://github.com/sweetticket"><img src="img/github.png" alt="github" id="github-icon"></a>
+                <a href="http://linkedin.com/in/yk494"><img src="img/linkedin.png" alt="linkedin" id="linkedin-icon"></a>
         <p id="footer-copyright">Yoon Jae Kim &copy; 2015</p>
 </footer>
     <!--Icons made by http://www.simpleicon.com -->
