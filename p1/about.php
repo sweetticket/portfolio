8adf593fdfb597aa6a5e4c22213460fa95068730
sweetticket <yk494@cornell.edu>
2015-02-02 23:24:17 -0500

    about

--- p1/about.php
+++ p1/about.php
@@ -12,6 +12,16 @@
     <?php include("template/nav.php"); ?>
     <div id="wrapper">
     <img id="about-bg" alt="about-bg" src="img/about-bg.jpg">
+        <div id="content">
+            <h1>About the Artist</h1>
+            <div id="description">
+            <p>Yoon Jae, who goes by the alias Jenny, is currently studying computer science at Cornell University. She grew up around Boston, Massachusetts before her parents moved to South Korea in 2013.</p>
+
+<p>While she enjoys creating websites, apps, and coding in general, drawing and painting has been almost a habit since her early years.</p>
+
+<p>She continues to find ways to combine these seemingly polar opposite worlds into one. She hopes that she'll have the leisure to explore digital art and UI/UX design in the future, while pursuing her passion for coding.</p>
+            </div>
+        </div>
     <?php include("template/footer.php"); ?>
     </div>
 </body>
